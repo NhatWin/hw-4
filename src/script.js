c3999445229bba8a3ee97e9a3630312310eefce3
NhatWin <nhat0933@gmail.com>
2022-10-27 23:46:29 -0500

    Added timer to js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
-const answers = document.querySelector()
+const COUNTDOWN_TIME = 30;
+
+const answers = document.querySelectorAll("button");
+const countdownSpan = document.querySelector("#countdown")
+
+// Timer
+function countDown() {
+  let i = COUNTDOWN_TIME;
+
+  const countDownInterval = setInterval(function() {
+    countdownSpan.textContent = i; i--;
+
+    if (i < 0) {
+      clearInterval(countDownInterval);
+    }
+  }, 1000);
+}
+
+countDown();
